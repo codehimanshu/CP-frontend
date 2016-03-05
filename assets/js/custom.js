03954092263980f30c844fface8b4a5ddc3a2c77
Prashant Chaudhary <prashantp099@gmail.com>
2016-03-05 10:41:39 +0530

    view of expanded menu and search in this done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -30,3 +30,39 @@ function positionFooter()
   }      
 }
 positionFooter(); 
+
+$(document).on('click','.filter-box .viewMore',function(){
+  var parent = $(this).closest('.filter-box');
+  var top = parent.outerHeight() - $(this).parent().outerHeight();
+  var left = $(this).parent().outerWidth()
+  var submenu = parent.find('.submenu');
+  if(submenu.is(':visible'))
+  {
+    submenu.fadeOut('normal');
+  }
+  else
+  {    
+    submenu.css({'top':top+'px','left':left+'px'});
+    submenu.fadeIn('normal');
+  }
+});
+/*-----------Hide element if clicked somewhere else----------------*/
+$(document).click(function(event) { 
+  if(!$(event.target).closest('.toggleMenu').length && !$(event.target).is('.toggleMenu') && !$(event.target).is('.filter-box .viewMore')) {
+    if($('.toggleMenu').is(":visible")) {
+        $('.toggleMenu').fadeOut('normal')
+    }
+  }        
+});
+/*--------------Search in submenu------------*/
+$( document ).ready(function() {
+  $('input[name="suggestion"]').keyup(function(){
+   var valThis = $(this).val().toLowerCase().trim();
+   var list = $(this).closest('.submenu').find('ul>li');
+   list.each(function(){
+     var text = $(this).find('label').text().toLowerCase().trim();
+     console.log(text,valThis,text.indexOf(valThis));
+     (text.indexOf(valThis) == 0) ? $(this).show() : $(this).hide();
+   });
+ });
+});
